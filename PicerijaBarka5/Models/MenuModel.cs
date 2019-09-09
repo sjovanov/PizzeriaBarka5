5865f024649d33904bbe20d65ae2de1d50e0115c
Стефан Јованов <stefan.jovanov98@gmail.com>
2019-09-09 22:45:09 +0200

    Added intial models.

---
+++ PicerijaBarka5/Models/MenuModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PicerijaBarka5.Models
+{
+    public class MenuModel
+    {
+        /// <summary>
+        /// The pizzas that are on the menu
+        /// </summary>
+        public List<PizzaModel> Pizzas { get; set; }
+        public List<IngredientModel> PizzeriaIngredients { get; set; }
+
+        MenuModel()
+        {
+            Pizzas = new List<PizzaModel>();
+            PizzeriaIngredients = new List<IngredientModel>();
+        }
+    }
+}
---
+++ PicerijaBarka5/Models/PizzaModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PicerijaBarka5.Models
+{
+    public class PizzaModel
+    {
+        /// <summary>
+        /// The name of the pizza
+        /// </summary>
+        [Required]
+        public String Name { get; set; }
+        /// <summary>
+        /// The price of the pizza. Price of pizza is sum of price multiplied by the quantity and the income coeficient.
+        /// </summary>
+        public double Price { get { return Ingredients.Sum(x => x.Price * x.QuantityPerSmallPizza/1000) * (1 + incomeCoeficient); } set { Price = value; } }
+        /// <summary>
+        /// List of ingredients in the pizza
+        /// </summary>
+        public List<IngredientModel> Ingredients { get; set; }
+        private readonly double incomeCoeficient = 0.15;
+
+        PizzaModel() {
+            Ingredients = new List<IngredientModel>();
+        }
+
+    }
+}
